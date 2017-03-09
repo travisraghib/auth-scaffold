30ef11bb3408a1f8f0dfdcb262b4adc5cc3a8eff
Travis Raghibzadeh <travisraghib@apple.com>
2017-03-08 17:45:04 -0800

    [feature] + routes

--- server/routes.js
+++ server/routes.js
@@ -13,6 +13,9 @@ module.exports = function(app) {
 
   app.use('/api/auth', require('./api/auth'));
 
+  app.get('/api/health', (req, res) => res.status(200));
+  app.get('/api/test', (req, res) => res.send('test'));
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
